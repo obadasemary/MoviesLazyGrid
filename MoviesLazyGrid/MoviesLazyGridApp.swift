075dc5803cfbb5e91253858055b017178a0851e1
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-27 03:14:19 +0300

    Initial Commit

---
+++ MoviesLazyGrid/MoviesLazyGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesLazyGridApp.swift
+//  MoviesLazyGrid
+//
+//  Created by Abdelrahman Mohamed on 27.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct MoviesLazyGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
