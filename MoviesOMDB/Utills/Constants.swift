ab181008289588418026eccddcc66b42bd3ef4b9
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-06 01:55:27 +0300

    Create Constants.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ MoviesOMDB/Utills/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MoviesOMDB
+//
+//  Created by Abdelrahman Mohamed on 5.10.2020.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let API_KEY = "a22cefd9"
+}
