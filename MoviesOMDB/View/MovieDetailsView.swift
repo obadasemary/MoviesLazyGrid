e8afffd2369757dde0001d065120fda8c5fcfd9e
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-06 04:07:10 +0300

    Update MovieDetailsView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- MoviesOMDB/View/MovieDetailsView.swift
+++ MoviesOMDB/View/MovieDetailsView.swift
@@ -35,16 +35,19 @@ struct MovieDetailsView: View {
     }
 }
 
+// MARK: - PREVIEW
+
 struct MovieDetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        MovieDetailsView(movie:
-                            Movie(
-                                title: "Guardians of the Galaxy Vol. 2",
-                                year: "2017",
-                                imdbID: "tt3896198",
-                                type: "movie",
-                                poster: "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"
-                            )
+        MovieDetailsView(
+            movie:
+                Movie(
+                    title: "Guardians of the Galaxy Vol. 2",
+                    year: "2017",
+                    imdbID: "tt3896198",
+                    type: "movie",
+                    poster: "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"
+                )
         )
     }
 }
