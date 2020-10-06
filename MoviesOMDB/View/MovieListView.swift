f1f2fa62e7a51684fee456fa7441baa69420cb4b
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-06 04:56:15 +0300

    Update MovieListView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- MoviesOMDB/View/MovieListView.swift
+++ MoviesOMDB/View/MovieListView.swift
@@ -11,10 +11,15 @@ struct MovieListView: View {
     
     // MARK: - PROPERTIES
     
+    let movies: [MovieViewModel]
+    
     // MARK: - BODY
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(self.movies, id: \.imdbId) { movie in
+            
+            MovieCellView(movie: movie)
+        }
     }
 }
 
@@ -22,6 +27,39 @@ struct MovieListView: View {
 
 struct MovieListView_Previews: PreviewProvider {
     static var previews: some View {
-        MovieListView()
+        MovieListView(
+            movies: [
+                MovieViewModel.init(
+                    movie:
+                        Movie(
+                            title: "Captain Marvel",
+                            year: "2020",
+                            imdbID: "tt4154664",
+                            type: "movie",
+                            poster: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ1bDkDLq-_bteASakhnC1XYwlkErFuqcof7KMhFpRwVhCTh1Vo"
+                        )
+                ),
+                MovieViewModel.init(
+                    movie:
+                        Movie(
+                            title: "Avengers: Endgame",
+                            year: "2019",
+                            imdbID: "tt4154796",
+                            type: "movie",
+                            poster: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQA_-tL18_rj9zEcjN6n41NEaJm-kRNF9UeOtvksZ4z_OW6jRA9"
+                        )
+                ),
+                MovieViewModel.init(
+                    movie:
+                        Movie(
+                            title: "Guardians of the Galaxy Vol. 2",
+                            year: "2017",
+                            imdbID: "tt3896198",
+                            type: "movie",
+                            poster: "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"
+                        )
+                )
+            ]
+        )
     }
 }
