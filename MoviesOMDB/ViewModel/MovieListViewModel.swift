8200d2c7957886739d1561b64fcc78ea801e53e4
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-06 05:37:54 +0300

    Update MovieListViewModel.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- MoviesOMDB/ViewModel/MovieListViewModel.swift
+++ MoviesOMDB/ViewModel/MovieListViewModel.swift
@@ -7,24 +7,34 @@
 
 import Foundation
 
-class MovieListViewModel: ObservableObject {
+class MovieListViewModel: ViewModelBase {
     
     @Published var moviesVM = [MovieViewModel]()
     let httpClient = HTTPClient()
     
     func searchByName(_ name: String) {
         
-        httpClient.getMoviesBy(search: name) { result in
+        if name.isEmpty {
+            return
+        }
+        
+        self.loadingState = .loading
+        
+        httpClient.getMoviesBy(search: name.trimmedAndEscaped()) { result in
             
             switch result {
             case .success(let movies):
                 if let movies = movies {
                     DispatchQueue.main.async {
                         self.moviesVM = movies.map(MovieViewModel.init)
+                        self.loadingState = .success
                     }
                 }
             case .failure(let error):
                 print(error.localizedDescription)
+                DispatchQueue.main.async {
+                    self.loadingState = .failed
+                }
             }
         }
     }
