c5d5197a8aaa1c6b8e6f692fc720977b7b6add76
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-06 04:07:19 +0300

    Update Constants.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- MoviesOMDB/Utills/Constants.swift
+++ MoviesOMDB/Utills/Constants.swift
@@ -10,4 +10,7 @@ import Foundation
 struct Constants {
     
     static let API_KEY = "a22cefd9"
+    
+    static let path = "http://www.omdbapi.com/?s="
+    static let apiKeyString = "&apikey="
 }
