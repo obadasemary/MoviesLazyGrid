1272d58a7ed06e014bb8bc667e06c61898d7681f
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-06 04:08:34 +0300

    Update MovieListScreen.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- MoviesOMDB/Screens/MovieListScreen.swift
+++ MoviesOMDB/Screens/MovieListScreen.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct MovieListScreen: View {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
+
+// MARK: - PREVIEW
 
 struct MovieListScreen_Previews: PreviewProvider {
     static var previews: some View {
