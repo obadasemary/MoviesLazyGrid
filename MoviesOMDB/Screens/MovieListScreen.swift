7ad3fed990ca32eb25747e67f1d004e6b30d876b
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-06 05:38:06 +0300

    Update MovieListScreen.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- MoviesOMDB/Screens/MovieListScreen.swift
+++ MoviesOMDB/Screens/MovieListScreen.swift
@@ -12,20 +12,41 @@ struct MovieListScreen: View {
     // MARK: - PROPERTIES
     
     @ObservedObject private var movieListVM: MovieListViewModel
+    @State private var movieName: String = ""
     
     init() {
         self.movieListVM = MovieListViewModel()
-        self.movieListVM.searchByName("batman")
     }
     
     // MARK: - BODY
     
     var body: some View {
         VStack {
-            MovieListView(movies: self.movieListVM.moviesVM)
-                .listStyle(InsetGroupedListStyle())
+            
+            TextField("Search", text: $movieName) { _ in
+                
+            } onCommit: {
+                self.movieListVM.searchByName(self.movieName)
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Spacer()
+                
                 .navigationBarTitle("Movies")
+            
+            if self.movieListVM.loadingState == .success {
+                MovieListView(movies: self.movieListVM.moviesVM)
+                    .listStyle(InsetGroupedListStyle())
+            } else if self.movieListVM.loadingState == .failed {
+                FailedView()
+            } else if self.movieListVM.loadingState == .loading {
+                LoadingView()
+                
+                Spacer()
+            }
+            
         }
+        .padding()
         .embadNavigationView()
     }
 }
