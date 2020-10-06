7ca03480c0f0204f5e94065bb0cfbe9fef00321c
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-06 04:07:30 +0300

    Create MovieListScreen.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ MoviesOMDB/Screens/MovieListScreen.swift
@@ -0,0 +1,20 @@
+//
+//  MovieListScreen.swift
+//  MoviesOMDB
+//
+//  Created by Abdelrahman Mohamed on 6.10.2020.
+//
+
+import SwiftUI
+
+struct MovieListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieListScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieListScreen()
+    }
+}
