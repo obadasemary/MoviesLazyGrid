ef9dc7e22aa32820bac429fff03e135b64afdd65
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-06 04:12:01 +0300

    Create MovieListView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ MoviesOMDB/View/MovieListView.swift
@@ -0,0 +1,27 @@
+//
+//  MovieListView.swift
+//  MoviesOMDB
+//
+//  Created by Abdelrahman Mohamed on 6.10.2020.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct MovieListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieListView()
+    }
+}
