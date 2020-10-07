d49a144db8e1d9e1529bacdc6b63a3b6556fe0d0
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-07 04:40:21 +0300

    Update URL+Extensions.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- MoviesOMDB/Extensions/URL+Extensions.swift
+++ MoviesOMDB/Extensions/URL+Extensions.swift
@@ -10,6 +10,10 @@ import Foundation
 extension URL {
     
     static func forMoviesByName(_ name: String) -> URL? {
-        return URL(string: Constants.path + name + Constants.apiKeyString + Constants.API_KEY)
+        return URL(string: Constants.pathS + name + Constants.apiKeyString + Constants.API_KEY)
+    }
+    
+    static func forMovieByImdbId(_ imdbId: String) -> URL? {
+        return URL(string: Constants.pathI + imdbId + Constants.apiKeyString + Constants.API_KEY)
     }
 }
