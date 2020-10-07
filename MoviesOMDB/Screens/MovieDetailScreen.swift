8d06deed1537176408b0c42ed4334c92d64420f2
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-07 04:52:31 +0300

    Create MovieDetailScreen.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ MoviesOMDB/Screens/MovieDetailScreen.swift
@@ -0,0 +1,27 @@
+//
+//  MovieDetailScreen.swift
+//  MoviesOMDB
+//
+//  Created by Abdelrahman Mohamed on 7.10.2020.
+//
+
+import SwiftUI
+
+struct MovieDetailScreen: View {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct MovieDetailScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetailScreen()
+    }
+}
