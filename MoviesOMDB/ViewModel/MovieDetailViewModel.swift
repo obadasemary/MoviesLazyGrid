e38e5eb4843c587669d741a4ba782c519125ff83
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-10-07 04:52:28 +0300

    Create MovieDetailViewModel.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ MoviesOMDB/ViewModel/MovieDetailViewModel.swift
@@ -0,0 +1,65 @@
+//
+//  MovieDetailViewModel.swift
+//  MoviesOMDB
+//
+//  Created by Abdelrahman Mohamed on 7.10.2020.
+//
+
+import Foundation
+
+class MovieDetailViewModel: ViewModelBase {
+    
+    private var movieDetail: MovieDetail?
+    
+    private var httpClient = HTTPClient()
+    
+    init(movieDetail: MovieDetail? = nil) {
+        
+        self.movieDetail = movieDetail
+    }
+    
+    func getDetailsByImdbId(imdbId: String) {
+        
+        self.loadingState = .loading
+        
+        httpClient.getMovieDetailsBy(imdbId: imdbId) { result in
+            
+            switch result {
+            
+            case .success(let details):
+                DispatchQueue.main.async {
+                    self.movieDetail = details
+                    self.loadingState = .success
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+                DispatchQueue.main.async {
+                    self.loadingState = .failed
+                }
+            }
+        }
+    }
+    
+    var title: String {
+        self.movieDetail?.title ?? ""
+    }
+    
+    var poster: String {
+        self.movieDetail?.poster ?? ""
+    }
+    
+    var plot: String {
+        self.movieDetail?.plot ?? ""
+    }
+    
+    var rating: Int {
+        get {
+            let ratingAsDouble = Double(self.movieDetail?.imdbRating ?? "0.0")
+            return Int(ceil(ratingAsDouble ?? 0.0))
+        }
+    }
+    
+    var director: String {
+        self.movieDetail?.director ?? ""
+    }
+}
